a6f05ce49cb5ea0afd6e9c338bda3633d4e22bb0
norrest <admin@obu4enie.kiev.ua>
2016-09-09 07:00:40 +0300

    Install mDNS fop Apple Router
    Install mDNS fop Apple Router

--- var/www/_footer.php
+++ var/www/_footer.php
@@ -23,6 +23,7 @@
 			<button id="free-space" name="Free Disk Space" formtarget="_blank" formaction="free_space.php" class="btn btn-primary btn-large btn-block"><i class=""></i> Free Disk Space </button>
 			<button id="check-and-space" name="Check internal disk" formtarget="_blank" formaction="chec.php" class="btn btn-primary btn-large btn-block"><i class=""></i> Checking internal Disk </button>
 			<button id="force-remove-samba" name="Forced Remove Network Mount" formtarget="_blank" formaction="del.php" class="btn btn-primary btn-large btn-block"><i class=""></i> Forced Removal Of Network Mount </button>
+			<button id="update-git-hub" name="mDNS add" formtarget="_blank" formaction="apple.php" class="btn btn-primary btn-large btn-block"><i class=""></i> Install mDNS fop Apple Router </button>
 			<button id="update-git-hub" name="Update Git Hub" formtarget="_blank" formaction="update.php" class="btn btn-primary btn-large btn-block"><i class=""></i> Update / Reinstall Updates </button>
 		</div>
 		<div class="modal-footer">
