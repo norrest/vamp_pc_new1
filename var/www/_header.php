825345a4f5f0a4b8c95aea73393e01687f5a2395
norrest <admin@obu4enie.kiev.ua>
2017-05-29 10:56:43 +0300

    Update _header.php

--- var/www/_header.php
+++ var/www/_header.php
@@ -93,7 +93,7 @@
 			<li class="<?php ami('net-config'); ?>"><a href="net-config.php"><i class="fa fa-sitemap sx"></i> Network</a></li>
 			<li class="<?php ami('settings'); ?>"><a href="settings.php"><i class="fa fa-wrench sx"></i> System</a></li>
 			<li><a href="#poweroff-modal" data-toggle="modal"><i class="fa fa-power-off sx"></i> Turn off</a></li>
-			<form method="post"><button id="stop-all" name="stop-all" class="btn btn-cmd" ><i class="fa">Restart MPD</i></button></form>
+			<form method="post"><button id="stop-all" name="stop-all" class="btn btn-cmd" >Restart MPD</button></form>
 		</ul>
 	</div>
 	<div class="playback-controls">	
